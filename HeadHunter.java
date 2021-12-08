5b16ed154c030cbfd4df8fde80d5181a1c687d30
FERDdeveloper <86313430+ferderplays@users.noreply.github.com>
2021-12-08 23:18:30 +0100

    1.0.0

---
+++ HeadHunter.java
@@ -0,0 +1,58 @@
+import java.io.File;
+
+public class HeadHunter {
+    /**
+     * @author <a href="https://github.com/ferderplays">ferderplays</a>
+     */
+    // stupid fool idea i had to add
+    boolean hacked;
+
+    public void isHacked(boolean hacked) {
+        hacked = !hacked;
+    }
+
+    public static void HeadHunter(boolean hacked) {
+        try {
+            for (int g = 0; g < 1; g++) {
+                // thing for the faggots on ohare.cc
+                String comon = "for all the people on ohare.cc";
+            }
+        } catch (Exception e) {
+            Exception h = new Exception("Nigga U R ratted! :D");
+        }
+    }
+
+    // Thread
+    public static Thread l;
+
+    public HeadHunter() {
+        // path number 1
+        final String path1 = System.getenv("AppData" + ".minecraft");
+        try {
+            // data from a String below
+            String data = path1;
+            // necaserry filename String to get java files(hack clients and more)
+            String filename = "";
+            // jarfile filter for files
+            File jarfile = new File(".jar" + filename);
+            // this gets mc account cache
+            File accountcache = new File("accountcache.json");
+            // this gets.... well.... accountcache file
+            System.getenv("AppData" + ".minecraft" + accountcache);
+            if (jarfile.exists()) {
+                // path number 2
+                String path2 = System.getenv("user.home" + "Desktop");
+                // this gets the jarfile
+                this.getFile(jarfile, accountcache);
+            }
+        }
+        // some sh!t
+        catch (Exception e) {
+            System.out.println("nigga u were ratted :D");
+        }
+    }
+
+    // i won't show how to make this work ever!
+    private void getFile(File jarfile, File accountcache) {
+    }
+}
